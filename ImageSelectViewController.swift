61c16d6b3033409c6aabb6596b076f7acb5ac29f
yasusi.bando <bando057@outlook.jp>
2025-06-20 11:07:09 +0900

    やり直し　成功

---
+++ ImageSelectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram2
+//
+//  Created by 坂東康史 on 2025/06/20.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
